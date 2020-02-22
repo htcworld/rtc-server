025170baa30202de39ea9cc8c4bb4838f5ddce11
htcworld <61133997+htcworld@users.noreply.github.com>
2020-02-22 07:52:07 +0400

    goo

--- routes/index.js
+++ routes/index.js
@@ -45,21 +45,14 @@ function getHDDefault(userAgent) {
 // iceServers will be filled in by the TURN HTTP request.
 function makePCConfig(iceTransports) {
   var pcConfig = {
-      'iceServers': [
-    {
-      'urls': 'stun:stun.l.google.com:19302'
-    },
-    {
-      'urls': 'turn:192.158.29.39:3478?transport=udp',
-      'credential': 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
-      'username': '28224511:1379330808'
-  },
-  {
-      'urls': 'turn:192.158.29.39:3478?transport=tcp',
-      'credential': 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
-      'username': '28224511:1379330808'
-   }
-  ]
+       iceServers : [
+    'urls': [
+        'stun:stun.l.google.com:19302',
+        'stun:stun1.l.google.com:19302',
+        'stun:stun2.l.google.com:19302',
+        'stun:stun.l.google.com:19302?transport=udp',
+    ]
+ ]
 };
   if (iceTransports) {
     config.iceTransports = iceTransports;
